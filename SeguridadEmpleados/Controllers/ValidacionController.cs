9da561c3e0654ec8b81666e4976a395e2e63e732
Jesús Giganto Diez <Jesus.Giganto@tajamar365.com>
2018-02-19 23:46:07 +0100

    pagina 5

---
+++ SeguridadEmpleados/Controllers/ValidacionController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using SeguridadEmpleados.Models;
+
+namespace SeguridadEmpleados.Controllers
+{
+    public class ValidacionController : Controller
+    {
+        // GET: Validacion
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(String apellido, int empno)
+        {
+            ValidateUsers validacion = new ValidateUsers();
+            if(validacion.ExisteUsuario(apellido,empno))
+            {
+                FormsAuthenticationTicket ticket =
+                    new FormsAuthenticationTicket(1, apellido, DateTime.Now,
+                    DateTime.Now.AddSeconds(30), true, validacion.Role,
+                    FormsAuthentication.FormsCookiePath);
+
+                String ticketcifrado = FormsAuthentication.Encrypt(ticket);
+
+                HttpCookie cookie = new HttpCookie("cookieusuario", ticketcifrado);
+                Response.Cookies.Add(cookie);
+                return RedirectToAction("Index", "Administracion");
+            }
+            else
+            {
+                ViewBag.Mensaje = "Usuario o PW incorrectos";
+            }
+            return View();
+             
+        }
+        public ActionResult ErrorAcceso()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SeguridadEmpleados/Models/ValidateUsers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SeguridadEmpleados.Models
+{
+    public class ValidateUsers
+    {
+        ModeloEmpleados modelo;
+        public ValidateUsers()
+        {
+            modelo = new ModeloEmpleados();
+        }
+            
+        public String Role { get; set; }
+
+        public bool ExisteUsuario(String apellido,int empno)
+        {
+            List<EMP> emp = modelo.GetEmpleados();
+            foreach(var usuario in emp)
+            {
+                if((usuario.APELLIDO==apellido && usuario.EMP_NO == empno) &&
+                    (usuario.OFICIO=="PRESIDENTE"||usuario.OFICIO== "DIRECTOR"))
+                {
+                    this.Role = "AMDMINISTRADOR";
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+}
+
+/*this.Role = "AMDMINISTRADOR";
+                        return true;
+ if((emp.Exists(z => z.APELLIDO == apellido)==true) && (emp.Exists(z => z.EMP_NO == empno) == true))*/
